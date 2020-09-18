2b68989cfb478ce11049a21cabc022f156fe72a4
MTaDirector <romainlagorsse@outlook.fr>
2020-09-18 10:32:09 +0200

    adding a date to all movie

--- components/Movie.js
+++ components/Movie.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const Movie = ({title, synopsis, ratings}) => {
+const Movie = ({title, synopsis, ratings, date}) => {
   return (
     <div>
       <h4>{ title }</h4>
       <p>{ synopsis }</p>
       <p>{ ratings }</p>
-
+      <p>{ date }</p>
     </div>
   )
 }
--- pages/movies.js
+++ pages/movies.js
@@ -6,16 +6,19 @@ const movies = [
     title: "terminator",
     synopsis: "Sit alias animi nemo numquam optio recusandae, a repudiandae velit commodi ullam dolorem. Dolore nemo natus harum distinctio quae. Ullam, dolore quaerat.",
     ratings: 4,
+    date: 1984,
   },
   {
     title: "terminator 2",
     synopsis: "Deserunt tempore eos nostrum assumenda iste ea mollitia rerum nemo. Nam exercitationem dolor neque vel beatae qui recusandae rem unde voluptatibus voluptate.",
     ratings: 5,
+    date: 1991,
   },
   {
     title: "terminator 3",
     synopsis: "Alias odio architecto aperiam vitae sapiente tenetur voluptas illum itaque velit iste. Dicta eius ullam cumque, quos quasi quaerat odit rerum officia.",
     ratings: 3,
+    date: 2003,
   },
 ]
 
@@ -31,6 +34,7 @@ const Movies = () => {
                   title={movie.title}
                   synopsis={movie.synopsis}
                   ratings={movie.ratings}
+                  date={movie.date}
                 />
               </li>
             )
